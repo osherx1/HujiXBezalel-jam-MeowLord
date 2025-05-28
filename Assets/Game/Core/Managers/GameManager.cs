d8f46c2ffe8a94e75d6cdd35dbb1c3a4192707cf
eladfirer <elad.firer@mail.huji.ac.il>
2025-05-28 16:08:32 +0300

    fixed something

--- Assets/Game/Core/Managers/GameManager.cs
+++ Assets/Game/Core/Managers/GameManager.cs
@@ -52,7 +52,7 @@ namespace Game.Core.Managers
 
         private IEnumerator GameFinishedTimerCoroutine()
         {
-            yield return new UnityEngine.WaitForSeconds(10f);
+            yield return new WaitForSeconds(60f);
             GameEvents.GameFinished();
             SceneManager.LoadScene("EndScene");
             yield return null;
