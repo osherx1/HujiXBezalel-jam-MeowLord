2e15604cdc5c7461589d248c10cdbf5796150896
eladfirer <elad.firer@mail.huji.ac.il>
2025-06-05 01:19:21 +0300

    conected number of segments to yarn counter animation

--- Assets/Game/UI/Scripts/NumberOfSegmentsRenderer.cs
+++ Assets/Game/UI/Scripts/NumberOfSegmentsRenderer.cs
@@ -7,21 +7,28 @@ namespace Game.UI.Scripts
     public class NumberOfSegmentsRenderer : MonoBehaviour
     {
         [SerializeField] private TextMeshProUGUI segmentsText;
+        [SerializeField] private Animator segmentsAnimator;
 
         private void OnEnable()
         {
             GameEvents.OnNumberOfSegmentsChanged += UpdateSegmentsText;
+            GameEvents.OnNumberOfSegmentsChanged += UpdateSegmentsAnimator;
         }
 
         private void OnDisable()
         {
             GameEvents.OnNumberOfSegmentsChanged -= UpdateSegmentsText;
+            GameEvents.OnNumberOfSegmentsChanged -= UpdateSegmentsAnimator;
         }
 
         private void UpdateSegmentsText(int value)
         {
             if (segmentsText != null)
                 segmentsText.text = $"Lines: {value}";
+        }
+        private void UpdateSegmentsAnimator(int value)
+        {
+            segmentsAnimator.SetInteger("Counter", value);
         }
     }
 } 
