630de30f0cb803cd8c65a09f64fd929dc131ea3c
osherx1 <oshermab@gmail.com>
2025-06-15 17:46:55 +0300

    update positions and layers for various game objects and add debug logs for better tracking

--- Assets/Game/Platforms/Prefabs/MovingPlatformsCode/OnIdleNoTrayEntered.cs
+++ Assets/Game/Platforms/Prefabs/MovingPlatformsCode/OnIdleNoTrayEntered.cs
@@ -4,6 +4,7 @@ public class OnIdleNoTrayEntered : StateMachineBehaviour
 {
     public override void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
+        Debug.Log("OnIdleNoTrayEntered: OnStateEnter called for " + animator.name);
         var mp = animator.GetComponentInParent<Game.Platforms.Scripts.MovingPlatform>();
         if (mp != null)
         {
