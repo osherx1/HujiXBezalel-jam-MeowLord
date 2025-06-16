eb4de3bfcaf5f6e4ab55a8c3c00ee73b54c639da
eladfirer <elad.firer@mail.huji.ac.il>
2025-06-16 13:30:30 +0300

    added sound manager

---
+++ Assets/Game/Core/Audio/AudioManager.cs
@@ -0,0 +1,33 @@
+using System;
+using Game.Core.Generics;
+using UnityEngine;
+
+namespace Game.Core.Audio
+{
+    public class AudioManager : MonoSingleton<AudioManager>
+    {
+        public Sound[] sounds;
+    
+        public SoundObject Play(AudioName name, Vector3 pos)
+        {
+            Sound s = Array.Find(sounds, sound => sound.name == name);
+            if (s == null)
+            {
+                Debug.LogWarning("Sound: " + name + " not found!");
+                return null;
+            }
+
+       
+
+            SoundObject soundObject = SoundPool.Instance.Get();
+        
+            soundObject.Play(s,pos);
+            return soundObject;
+        }
+    
+    }
+
+    public enum AudioName
+    {
+    }
+}
